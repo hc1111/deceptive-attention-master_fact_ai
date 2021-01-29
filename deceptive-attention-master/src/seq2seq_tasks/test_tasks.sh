f72b581f1b7722d63612647802ba4dcb559641aa
Sander Kohnstamm <sanderkohnstamm@gmail.com>
2021-01-29 14:00:06 +0100

    adding check logs notebook and test bash files

--- deceptive-attention-master/src/seq2seq_tasks/test_tasks.sh
+++ deceptive-attention-master/src/seq2seq_tasks/test_tasks.sh
@@ -1,10 +1,10 @@
 mkdir -p logs
 mkdir -p data/models
 mkdir -p data/vocab
-for seed in `seq 1 5`; do
+for seed in `seq 1 2`; do
    for coeff in 0.0 1.0 0.1; do
 	for task in binary-flip rev copy en-de; do
-            unbuffer python3.6 -u train.py --task $task --epochs 30 --loss-coef $coeff --seed $seed --batch-size 128  | tee -a logs/task=$task\_coeff=$coeff\_seed=$seed.txt;
+            unbuffer python3.6 -u train.py --task $task --epochs 1 --loss-coef $coeff --seed $seed --batch-size 128  | tee -a logs/task=$task\_coeff=$coeff\_seed=$seed.txt;
             echo completed the config $task, coeff: $coeff, seed: $seed num;
         done;
     done;
